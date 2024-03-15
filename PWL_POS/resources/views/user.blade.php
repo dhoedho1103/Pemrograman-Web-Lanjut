ab6217b49d3b8eff052d16e6fc9d1aba62476cde
dhoedho1103 <dhoedho1103@gmail.com>
2024-03-15 23:36:10 +0700

    Update Pertemuan 4

--- PWL_POS/resources/views/user.blade.php
+++ PWL_POS/resources/views/user.blade.php
@@ -5,6 +5,7 @@
     </head>
     <body>
         <h1> Data User</h1>
+        <a href="/user/tambah">+ Tambah User</a>
         <table border="1" cellpadding="2" cellspasing="0">
             <tr>
                 <th>ID</th>
@@ -12,14 +13,15 @@
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            
+            @foreach ($data as $d)
             <tr>
-                <td>{{$data->id_user}}</td>
-                <td>{{$data->username}}</td>
-                <td>{{$data->nama}}</td>
-                <td>{{$data->level_id}}</td>
+                <td>{{$d->id_user}}</td>
+                <td>{{$d->username}}</td>
+                <td>{{$d->nama}}</td>
+                <td>{{$d->level_id}}</td>
+                <td><a href="/user/ubah/{{$d->id_user}}">Ubah</a> | <a href="/user/hapus/{{$d->id_user}}">Hapus</a></td>
             </tr>
-            
+            @endforeach
         </table>
     </body>
 </html>
