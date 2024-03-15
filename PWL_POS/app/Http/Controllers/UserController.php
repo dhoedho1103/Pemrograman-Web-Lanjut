64183b83fabb486d603894fffcaffd12226db592
dhoedho1103 <dhoedho1103@gmail.com>
2024-03-15 09:57:45 +0700

    Pertemuan 4 - Praktikum 2.5

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -80,7 +80,7 @@ class UserController extends Controller
     //     return view('user', ['data' => $user]);
     // }
 
-                                  //Praktikum 2.3
+                                  //Praktikum 2.4
         //langkah 1
         // public function index(){
         // $user = UserModel::firstOrCreate(
@@ -124,16 +124,65 @@ class UserController extends Controller
         // }
 
         //langkah 10
-        public function index(){
-            $user = UserModel::firstOrNew(
-                ['username' => 'manager33',
-                'nama' => 'Manager Tiga Tiga',
-                'password' => Hash::make('12345'),
-                'level_id' => 2
-                ],
-            );
-            $user->save();
+        // public function index(){
+        //     $user = UserModel::firstOrNew(
+        //         ['username' => 'manager33',
+        //         'nama' => 'Manager Tiga Tiga',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2
+        //         ],
+        //     );
+        //     $user->save();
 
-            return view('user', ['data' => $user]);
+        //     return view('user', ['data' => $user]);
+        // }
+
+                                          //Praktikum 2.5
+        //langkah 1
+        // public function index(){
+        //     $user = UserModel::create([
+        //         'username' => 'manager55',
+        //         'nama' => 'Manager55',
+        //         'password' => Hash::make('12345'),
+        //         'level_id' => 2,
+        //     ]);
+        //     $user->username = 'manager56';
+        
+        // $user->isDirty(); //true
+        // $user->isDirty('username'); //true
+        // $user->isDirty('nama'); //false
+        // $user->isDirty(['nama', 'username']); //true
+
+        // $user->isClean(); //false
+        // $user->isClean('username'); //false
+        // $user->isClean('nama'); //true
+        // $user->isClean(['nama', 'username']); //false
+
+        // $user->save();
+
+        // $user->isDirty(); //false
+        // $user->isClean(); //true
+        // dd($user->isDirty());
+
+        // }
+
+        //langkah 2
+        public function index(){
+            $user = UserModel::create([
+                'username' => 'manager11',
+                'nama' => 'Manager11',
+                'password' => Hash::make('12345'),
+                'level_id' => 2,
+            ]);
+            $user->username = 'manager12';
+        
+        $user->save();
+        
+        $user->wasChanged(); //true
+        $user->wasChanged('username'); //true
+        $user->wasChanged('nama'); //false
+        $user->wasChanged(['nama', 'username']); //true
+        dd($user->wasChanged(['nama', 'username'])); //true
         }
+        
 }
